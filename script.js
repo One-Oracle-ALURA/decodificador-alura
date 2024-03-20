bef9899eb5b10e5c80e859aeafb64472b209690a
XavierLimaTI <106970733+XavierLimaTI@users.noreply.github.com>
2024-03-20 01:41:53 -0300

    Projeto finalizado

--- script.js
+++ script.js
@@ -3,50 +3,55 @@ function converterTexto() {
     var operacao = document.querySelector('input[name="operacao"]:checked').value;
     var textoSaida = "";
   
-    if (operacao === "criptografar") {
-      textoSaida = criptografarTexto(textoEntrada);
-    } else {
-      textoSaida = descriptografarTexto(textoEntrada);
-    }
+        if (operacao === "criptografar") {
+            textoSaida = criptografarTexto(textoEntrada);
+            } else {
+            textoSaida = descriptografarTexto(textoEntrada);
+            }
+        
+            document.getElementById("output-text").value = textoSaida;
+}
   
-    document.getElementById("output-text").value = textoSaida;
-  }
-  
-  function criptografarTexto(texto) {
+function criptografarTexto(texto) {
     return texto.replace(/e/g, "enter")
                .replace(/i/g, "imes")
                .replace(/a/g, "ai")
                .replace(/o/g, "ober")
                .replace(/u/g, "ufat");
-  }
+}
   
-  function descriptografarTexto(texto) {
+function descriptografarTexto(texto) {
     return texto.replace(/enter/g, "e")
                .replace(/imes/g, "i")
                .replace(/ai/g, "a")
                .replace(/ober/g, "o")
                .replace(/ufat/g, "u");
-  }
+}
   
-  function copiarParaAreaTransferencia() {
+function copiarParaAreaTransferencia() {
     var textoSaida = document.getElementById("output-text");
     textoSaida.select();
     document.execCommand("copy");
     alert("Texto copiado para a área de transferência!");
-  }
+}
   
-  function alternarOperacao() {
+function alternarOperacao() {
     var radios = document.getElementsByName("operacao");
-    for (var i = 0; i < radios.length; i++) {
-      if (radios[i].checked) {
-        radios[i].checked = false;
-        if (i + 1 < radios.length) {
-          radios[i + 1].checked = true;
-        } else {
-          radios[0].checked = true;
+        for (var i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            radios[i].checked = false;
+            if (i + 1 < radios.length) {
+            radios[i + 1].checked = true;
+            } else {
+            radios[0].checked = true;
+            }
+            break;
         }
-        break;
-      }
     }
+}
+
+function limparCampos() {
+    document.getElementById("input-text").value = "";
+    document.getElementById("output-text").value = "";
   }
   
