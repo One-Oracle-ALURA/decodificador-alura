21090f80e62b42da5286f0f5d487c5bc5f050e71
XavierLimaTI <106970733+XavierLimaTI@users.noreply.github.com>
2024-03-20 01:07:20 -0300

    Update css

--- script.js
+++ script.js
@@ -1,37 +1,52 @@
-function convertText() {
-    var inputText = document.getElementById("input-text").value.toLowerCase();
-    var operation = document.querySelector('input[name="operation"]:checked').value;
-    var outputText = "";
+function converterTexto() {
+    var textoEntrada = document.getElementById("input-text").value.toLowerCase();
+    var operacao = document.querySelector('input[name="operacao"]:checked').value;
+    var textoSaida = "";
   
-    if (operation === "encrypt") {
-      outputText = encryptText(inputText);
+    if (operacao === "criptografar") {
+      textoSaida = criptografarTexto(textoEntrada);
     } else {
-      outputText = decryptText(inputText);
+      textoSaida = descriptografarTexto(textoEntrada);
     }
   
-    document.getElementById("output-text").value = outputText;
+    document.getElementById("output-text").value = textoSaida;
   }
   
-  function encryptText(text) {
-    return text.replace(/e/g, "enter")
+  function criptografarTexto(texto) {
+    return texto.replace(/e/g, "enter")
                .replace(/i/g, "imes")
                .replace(/a/g, "ai")
                .replace(/o/g, "ober")
                .replace(/u/g, "ufat");
   }
   
-  function decryptText(text) {
-    return text.replace(/enter/g, "e")
+  function descriptografarTexto(texto) {
+    return texto.replace(/enter/g, "e")
                .replace(/imes/g, "i")
                .replace(/ai/g, "a")
                .replace(/ober/g, "o")
                .replace(/ufat/g, "u");
   }
   
-  function copyToClipboard() {
-    var outputText = document.getElementById("output-text");
-    outputText.select();
+  function copiarParaAreaTransferencia() {
+    var textoSaida = document.getElementById("output-text");
+    textoSaida.select();
     document.execCommand("copy");
     alert("Texto copiado para a área de transferência!");
+  }
+  
+  function alternarOperacao() {
+    var radios = document.getElementsByName("operacao");
+    for (var i = 0; i < radios.length; i++) {
+      if (radios[i].checked) {
+        radios[i].checked = false;
+        if (i + 1 < radios.length) {
+          radios[i + 1].checked = true;
+        } else {
+          radios[0].checked = true;
+        }
+        break;
+      }
+    }
   }
   
